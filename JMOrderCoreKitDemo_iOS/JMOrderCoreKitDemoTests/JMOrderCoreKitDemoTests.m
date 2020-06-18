c21144fc74d1c1422cb1e34d671a2449e9df967b
lzj <lizhijian@jimilab.com>
2020-06-18 14:17:37 +0800

    iOS Demo for 1.0.5

---
+++ JMOrderCoreKitDemo_iOS/JMOrderCoreKitDemoTests/JMOrderCoreKitDemoTests.m
@@ -0,0 +1,37 @@
+//
+//  JMOrderCoreKitDemoTests.m
+//  JMOrderCoreKitDemoTests
+//
+//  Created by lzj<lizhijian_21@163.com> on 2020/4/21.
+//  Copyright © 2020 Jimi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JMOrderCoreKitDemoTests : XCTestCase
+
+@end
+
+@implementation JMOrderCoreKitDemoTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
